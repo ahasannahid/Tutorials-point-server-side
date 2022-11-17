7ff3c35e2e35df46c7f2d29028be8194bae8a657
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-17 20:36:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json')
+
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;   
+        const category_course = courses.filter(course => course.category_id === id);
+        res.send(category_course);
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+    console.log('tutorial server running on port', port);
+})
